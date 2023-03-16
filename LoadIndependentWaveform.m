ec61fcb1122502536e7bbe9026e679990369dd5f
antonioleeps <42138581+antonioleeps@users.noreply.github.com>
2023-03-16 15:40:22 +0100

    Update LoadIndependentWaveform.m

--- LoadIndependentWaveform.m
+++ LoadIndependentWaveform.m
@@ -4,14 +4,34 @@ global TaskParameters
 
 fs = Player.SamplingRate;
 
-PunishSound = rand(1, fs*TaskParameters.GUI.EarlyWithdrawalTimeOut)*2 - 1;
-SoundIndex = 1;
-Player.loadWaveform(SoundIndex, PunishSound);
+if TaskParameters.GUI.EarlyWithdrawalTimeOut > 0
+    PunishSound = rand(1, fs*TaskParameters.GUI.EarlyWithdrawalTimeOut)*2 - 1;
+    SoundIndex = 1;
+    
+    if ~isempty(PunishSound)
+        if isfield(BpodSystem.ModuleUSB, 'WavePlayer1')
+            Player.loadWaveform(SoundIndex, PunishSound);
+            Player.TriggerProfiles(SoundIndex, 1:2) = SoundIndex;
+        elseif isfield(BpodSystem.ModuleUSB, 'HiFi1')
+            Player.load(SoundIndex, PunishSound);
+        end
+    end
+end
 
-SoundLevel = 0.8;
-ErrorSound = rand(1, fs*TaskParameters.GUI.EarlyWithdrawalTimeOut)*2 - 1; 
-% ErrorSound = ErrorSound * SoundLevel;
-SoundIndex = 2;
-Player.loadWaveform(SoundIndex, ErrorSound);
+if TaskParameters.GUI.IncorrectChoiceTimeOut > 0
+    SoundLevel = 0.8;
+    ErrorSound = rand(1, fs*TaskParameters.GUI.IncorrectChoiceTimeOut)*2 - 1; 
+    % ErrorSound = ErrorSound * SoundLevel;
+    SoundIndex = 2;
+    
+    if ~isempty(ErrorSound)
+        if isfield(BpodSystem.ModuleUSB, 'WavePlayer1')
+            Player.loadWaveform(SoundIndex, ErrorSound);
+            Player.TriggerProfiles(SoundIndex, 1:2) = SoundIndex;
+        elseif isfield(BpodSystem.ModuleUSB, 'HiFi1')
+            Player.load(SoundIndex, ErrorSound);
+        end
+    end
+end 
 
 end
