c777f4fd0adec6924fefef4766f02de9afcd6238
OttLabIT <116281282+OttLabIT@users.noreply.github.com>
2023-04-11 19:24:55 +0200

    patched InitializeNidaq so that photometry figures can be saved
    this function needs to be depreciated so that the Bpod version prevails, but in the meantime, this has the same changes that Bpod got.

--- InitializeNidaq.m
+++ InitializeNidaq.m
@@ -1,6 +1,7 @@
 function [FigNidaq1,FigNidaq2]=InitializeNidaq()
 %% NIDAQ Initialization and Plots
 
+global BpodSystem
 global TaskParameters
 
 if (TaskParameters.GUI.DbleFibers+TaskParameters.GUI.Isobestic405+TaskParameters.GUI.RedChannel)*TaskParameters.GUI.Photometry >1
@@ -17,5 +18,8 @@ else
     FigNidaq2=[];
 end
 
+% temp patch for the sake of saving plots
+BpodSystem.GUIHandles.Nidaq1=FigNidaq1;
+BpodSystem.GUIHandles.Nidaq2=FigNidaq2;
 
 end  % InitializeNidaq()
